fd9a39f0b21a96478c540699723e1c58175fd509
AguSSantos Prieto Atienza <131393036+aguSSantos023@users.noreply.github.com>
2026-04-29 19:41:39 +0200

    chore: setup initial de mobile angular 21 + capacitor + daisyui

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.santos.dailygrid',
+  appName: 'DailyGrid',
+  webDir: 'dist/app/browser'
+};
+
+export default config;
